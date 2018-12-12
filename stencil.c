f2e00df81900cbe78eb77fc3751e01fe06069f30
MichaelRol <michael.rollins@hotmail.co.uk>
2018-12-12 18:12:15 +0000

    This is getting painful

--- stencil.c
+++ stencil.c
@@ -119,7 +119,7 @@ void stencil(const int nx, const int ny, float *  image, float * tmp_image, int 
 
     //left side column - FIX
     for(int j = 1; j < nx - 1; ++j){
-        printf((firstrow + j) * nx);
+        printf("%d",(firstrow + j) * nx);
         tmp_image[(firstrow + j) * nx] = image[(firstrow + j) * nx] * 0.6f;// + (image[(firstrow + j - 1) * nx] + image[(firstrow + j + 1) * nx] + image[(firstrow + j) * nx + 1]) * 0.1f;
     }
 
