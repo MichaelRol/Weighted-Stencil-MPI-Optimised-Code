d9d548f5d17760da3f57f8cd38670c72e4d16281
MichaelRol <michael.rollins@hotmail.co.uk>
2018-12-13 00:34:17 +0000

    Stitching images together

--- stencil.c
+++ stencil.c
@@ -84,7 +84,7 @@ int main(int argc, char* argv[]) {
         MPI_Send(image, nx * ny, MPI_FLOAT, MASTER, 123, MPI_COMM_WORLD);
     } else {
         for (int i = 1; i < size; i++) {
-            MPI_Recv(tmp_image, ny * nx, MPI_FLOAT, i, 123, MPI_COMM_WORLD, status);
+            MPI_Recv(tmp_image, ny * nx, MPI_FLOAT, i, 123, MPI_COMM_WORLD, &status);
             if (i != size - 1) {
                 for (int j = 0; j < (lastrow + 1) * nx - 1; j++){
                     image[i * nx * firstrow + j] = tmp_image[i * nx * firstrow + j];
