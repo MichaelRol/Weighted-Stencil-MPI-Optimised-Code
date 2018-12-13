f16a9ce6830921a11f800f9b2f09d84b068cfeff
MichaelRol <michael.rollins@hotmail.co.uk>
2018-12-13 19:24:03 +0000

    Getting it to work for 1 process

--- stencil.c
+++ stencil.c
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
     recvlargebuf = malloc(sizeof(float) * (lastrow + 1) * ny - 1);
 
     init_image(nx, ny, image, tmp_image);
-    // if (size != 1) {
+    if (size != 1) {
         double tic = wtime();
         for (int t = 0; t < niters; t++) {
             stencil(nx, ny, image, tmp_image, firstrow, lastrow, sendbuf, recvbuf, above, below, status, rank);
@@ -75,35 +75,35 @@ int main(int argc, char* argv[]) {
         }
         double toc = wtime();
         printf("%lf \n", toc-tic);
-    // } else {
-    //     double tic = wtime();
-    //     for (int t = 0; t < niters; t++) {
-    //         stencil1node(nx, ny, image, tmp_image);
-    //         stencil1node(nx, ny, tmp_image, image);
-    //     }
-    //     double toc = wtime();
-    //     printf("%lf \n", toc-tic);
-    // }
+    } else {
+        double tic = wtime();
+        for (int t = 0; t < niters; t++) {
+            stencil1node(nx, ny, image, tmp_image);
+            stencil1node(nx, ny, tmp_image, image);
+        }
+        double toc = wtime();
+        printf("%lf \n", toc-tic);
+    }
     free(sendbuf);
     free(recvbuf);
     
-    // if (rank != MASTER) {
-    //     MPI_Send(image, nx * ny, MPI_FLOAT, MASTER, 123, MPI_COMM_WORLD);
-    // } else {
-    //     for (int i = 1; i < size; i++) {
-    //         MPI_Recv(tmp_image, ny * nx, MPI_FLOAT, i, 123, MPI_COMM_WORLD, &status);
-    //         if (i != size - 1) {
-    //             for (int j = 0; j < (lastrow + 1) * nx + 1; j++){
-    //                 image[i * nx * local_nrows + j] = tmp_image[i * nx * local_nrows + j];
-    //             }
-    //         } else {
-    //             output_image("pls.pgm", nx, ny, image);
-    //             for (int j = (lastrow + 1) * i * nx ; j < (nx) * (ny - 1) + nx; j++){
-    //                 image[j] = tmp_image[j];
-    //             }
-    //         }
-    //     }
-    // }
+    if (rank != MASTER && size != 1) {
+        MPI_Send(image, nx * ny, MPI_FLOAT, MASTER, 123, MPI_COMM_WORLD);
+    } else if (size != 1) {
+        for (int i = 1; i < size; i++) {
+            MPI_Recv(tmp_image, ny * nx, MPI_FLOAT, i, 123, MPI_COMM_WORLD, &status);
+            if (i != size - 1) {
+                for (int j = 0; j < (lastrow + 1) * nx + 1; j++){
+                    image[i * nx * local_nrows + j] = tmp_image[i * nx * local_nrows + j];
+                }
+            } else {
+                output_image("pls.pgm", nx, ny, image);
+                for (int j = (lastrow + 1) * i * nx ; j < (nx) * (ny - 1) + nx; j++){
+                    image[j] = tmp_image[j];
+                }
+            }
+        }
+    }
 
     for (int i = 0; i < nx; i++) {
         printf("Row: %d, Val; %f\n", i, image[(i + 1) * nx - 1]);
@@ -123,7 +123,7 @@ void stencil(const int nx, const int ny, float * restrict image, float * restric
     for (int i = 0; i < nx; i++) {
         sendbuf[i] = image[lastrow * nx + i];
     }
-    MPI_Ssend(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
+    MPI_Send(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
     MPI_Recv(recvbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD, &status);
 
       //if top section
@@ -139,7 +139,7 @@ void stencil(const int nx, const int ny, float * restrict image, float * restric
         //top right cell
         tmp_image[nx - 1] = image[nx - 1] * 0.6f + (image[nx - 2] + image[2 * nx - 1]) * 0.1f;
         
-    MPI_Ssend(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
+    MPI_Send(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
     MPI_Recv(recvbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD, &status);
     //any other section
     } else {
@@ -178,7 +178,7 @@ void stencil(const int nx, const int ny, float * restrict image, float * restric
         sendbuf[i] = image[firstrow * nx + i];
     }
 
-    MPI_Ssend(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
+    MPI_S   end(sendbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD);
     MPI_Recv(recvbuf, nx, MPI_FLOAT, 0, 123, MPI_COMM_WORLD, &status);
 
     //if last section
@@ -210,6 +210,49 @@ void stencil(const int nx, const int ny, float * restrict image, float * restric
         tmp_image[(lastrow + 1) * nx - 1] = image[(lastrow + 1) * nx - 1] * 0.6f + (image[(lastrow + 1) * nx - 2] + image[(lastrow) * nx - 1] + recvbuf[nx - 1]) * 0.1f;
     }
 
+
+}
+
+void stencil1node(const int nx, const int ny, float * restrict  image, float * restrict tmp_image) {
+
+    //top left cell
+    tmp_image[0] = image[0] * 0.6f + (image[ny] + image[1]) * 0.1f;
+
+    //top row
+    for(int i = 1; i < ny - 1; ++i){
+        tmp_image[i] = image[i] * 0.6f + (image[i - 1] + image[i + 1] + image[ny + i]) * 0.1f;
+    }
+
+    //top right cell
+    tmp_image[ny-1] = image[ny-1] * 0.6f + (image[ny-2] + image[2*  ny-1]) * 0.1f;
+
+    //left side column
+    for(int j = 1; j < ny - 1; ++j){
+        tmp_image[ny * j] = image[ny * j] * 0.6f + (image[ny * (j - 1)] + image[ny * (j + 1)] + image[ny * j + 1]) * 0.1f;
+    }
+
+    //right side column
+    for(int j = 1; j < ny - 1; ++j){
+        tmp_image[ny * j + ny - 1] = image[ny * j + ny - 1] * 0.6f + (image[ny * (j - 1) + ny - 1] + image[ny * (j + 1) + ny - 1] + image[ny * j + ny - 2]) * 0.1f;
+    }
+
+    //inner grid
+    for (int i = 1; i < ny - 1; ++i) {
+        for (int j = 1; j < nx - 1; ++j) {
+            tmp_image[j+i*ny] = image[j+i*ny] * 0.6f + (image[j  +(i-1)*ny] + image[j  +(i+1)*ny] + image[j-1+i*ny] + image[j+1+i*ny]) * 0.1f;
+        }
+    }
+
+    //bottom left cell
+    tmp_image[(ny-1) * ny] = image[(ny-1) * ny] * 0.6f + (image[(ny-2) * ny] + image[1 + (ny-1) * ny]) * 0.1f;
+
+    //bottom row
+    for(int i = 1; i < ny - 1; ++i){
+        tmp_image[(ny - 1) * ny + i] = image[(ny - 1) * ny + i] * 0.6f + (image[(ny - 1) * ny + (i - 1)] + image[(ny - 1) * ny + (i + 1)] + image[(ny - 2) * ny + i]) * 0.1f;
+    }
+
+    //bottom right cell
+    tmp_image[(ny - 1) + (ny - 1) * ny] = image[(ny - 1) + (ny - 1) * ny] * 0.6f + (image[(ny - 2) + (ny - 1) * ny] + image[(ny - 1) + (ny - 2) * ny]) * 0.1f;
 
 }
 
